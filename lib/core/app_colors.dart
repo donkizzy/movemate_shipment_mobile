0d343e7138f5238346466dc011b4e86afc327dd5
Amos Godwin <amosgodwin500@gmail.com>
2023-09-15 17:49:04 +0100

    added major app colors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+
+
+import 'dart:ui';
+
+const gigasPurple = Color(0xFF553A9C);
+const jaffaOrange = Color(0xFFf68b32);
+const buntingBlue = Color(0xFF141e40);
+const spunPearlGrey = Color(0xFFb3b2b4);
+const hintGrey = Color(0xFFfbfafb);
