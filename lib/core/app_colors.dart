9df98b4a85b65f044066f08219f11e828f9cb34c
Amos Godwin <amosgodwin500@gmail.com>
2023-09-16 20:01:57 +0100

    added new colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -7,3 +7,6 @@ const jaffaOrange = Color(0xFFf68b32);
 const buntingBlue = Color(0xFF141e40);
 const spunPearlGrey = Color(0xFFb3b2b4);
 const hintGrey = Color(0xFFfbfafb);
+const shipGrey = Color(0xFF3B383C);
+const bombayGrey = Color(0xFFADADAE);
+const appleGreen = Color(0xFF4CC734);
